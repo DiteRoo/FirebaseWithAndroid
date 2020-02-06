3ce0394849adb45f2715409ba7f818df87ec81ef
Fazt <contact@faztweb.com>
2020-02-05 21:19:42 -0300

    SignOut Authentication

--- app/src/main/java/com/example/firebase02/SegundaActivity.java
+++ app/src/main/java/com/example/firebase02/SegundaActivity.java
@@ -2,13 +2,31 @@ package com.example.firebase02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SegundaActivity extends AppCompatActivity {
+
+
+    private Button myLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda);
+
+        myLogout = (Button)findViewById(R.id.logout);
+
+        myLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(SegundaActivity.this,MainActivity.class ));
+            }
+        });
     }
 }
