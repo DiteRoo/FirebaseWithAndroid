aec3fbe6d76b1a9404e01cad295c7b04f4f7409b
Fazt <contact@faztweb.com>
2020-02-08 00:41:47 -0300

    Login Facebook

--- app/src/main/java/com/example/firebase02/MainActivity.java
+++ app/src/main/java/com/example/firebase02/MainActivity.java
@@ -10,15 +10,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 //import com.google.firebase.auth.AuthResult;
 //import com.google.firebase.auth.FirebaseAuth;
 //import com.google.firebase.auth.FirebaseUser;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,10 +35,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "Antut";
+    private static final String TAG = "Error";
 
     TextView texto;
     EditText mensaje;
@@ -37,6 +50,11 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText mEmailField, mPasswordField;
     private Button myLoginButton;
+
+    //private Button myLoginFacebook;
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private ProgressBar progressBar;
 
     // Initialize Firebase Auth
     private FirebaseAuth mAuth;
@@ -48,19 +66,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mAuth = FirebaseAuth.getInstance();
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         mEmailField = (EditText) findViewById(R.id.email);
         mPasswordField = (EditText) findViewById(R.id.password);
         myLoginButton = (Button) findViewById(R.id.login);
 
+        loginButton = (LoginButton) findViewById(R.id.loginButton);
+        callbackManager = CallbackManager.Factory.create();
+
+
+        loginButton.setReadPermissions(Arrays.asList("email"));
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Cancel", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(getApplicationContext(),"Error", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-
-                if (firebaseAuth.getCurrentUser() != null) {
-                    startActivity(new Intent(MainActivity.this, SegundaActivity.class));
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!= null) {
+                    goMainScreen();
                 }
             }
         };
@@ -71,15 +116,43 @@ public class MainActivity extends AppCompatActivity {
                 LogearUsuario();
             }
         });
+        Toast.makeText(this,"Create",Toast.LENGTH_LONG).show();
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this, SegundaActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+        Toast.makeText(this,"Start",Toast.LENGTH_LONG).show();
 
         mAuth.addAuthStateListener(mAuthListener);
 
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        //Toast.makeText(this,"Pause",Toast.LENGTH_LONG).show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+        Toast.makeText(this,"Stop",Toast.LENGTH_LONG).show();
     }
 
     private void LogearUsuario(){
@@ -109,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-                            //Log.w(TAG, "signInWithEmail", task.getException());
+                            Log.w(TAG, "signInWithEmail", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
@@ -117,6 +190,23 @@ public class MainActivity extends AppCompatActivity {
                         // ...
                     }
                 });
+    }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        progressBar.setVisibility(View.VISIBLE);
+        loginButton.setVisibility(View.GONE);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Error login", Toast.LENGTH_LONG).show();
+                }
+                progressBar.setVisibility(View.GONE);
+                loginButton.setVisibility(View.VISIBLE);
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/firebase02/SegundaActivity.java
+++ app/src/main/java/com/example/firebase02/SegundaActivity.java
@@ -3,16 +3,25 @@ package com.example.firebase02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SegundaActivity extends AppCompatActivity {
 
 
     private Button myLogout;
+
+    private TextView nameTextView;
+    private TextView emailTextView;
+    private TextView uidTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +30,41 @@ public class SegundaActivity extends AppCompatActivity {
 
         myLogout = (Button)findViewById(R.id.logout);
 
+        nameTextView = (TextView) findViewById(R.id.nameTextView);
+        emailTextView = (TextView) findViewById(R.id.emailTextView);
+        uidTextView = (TextView) findViewById(R.id.uidTextView);
+
+        /*if (AccessToken.getCurrentAccessToken() == null) {
+            goLoginScreen();
+        }*/
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+            String uid = user.getUid();
+
+            nameTextView.setText(name);
+            emailTextView.setText(email);
+            uidTextView.setText(uid);
+        }
+
+
         myLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(SegundaActivity.this,MainActivity.class ));
+                LoginManager.getInstance().logOut();
+                goLoginScreen();
             }
         });
+    }
+
+    private void goLoginScreen() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
